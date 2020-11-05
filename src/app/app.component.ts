8d6b33b2035864004e3ea717b5031974f3ccf6d7
luugm <luisafergm@gmail.com>
2020-11-05 15:45:10 -0600

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Item {
+	description:string;
+	checked:boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EjercicioAngular';
+
+	private items: Item[] = [];
+
+	addItem(description: string){
+		this.items.push({description, checked: false});
+	}
+
+	removeItem(item: Item){
+		let index = this.items.indexOf(item);
+		if(index > -1){
+			this.items.splice(index,1);
+		}
+	}
 }
